21e071eae3b5ee7fad2269d121d6ef3fb19904b0
mallynGit <miguelitorruiz@gmail.com>
2024-05-20 09:11:05 +0200

    logueo errores para hosting

--- src/index.js
+++ src/index.js
@@ -11,40 +11,40 @@ const __dirname = path.resolve() + '/src'
 const app = express()
 dotenv.config()
 
-app.use(express.json())
-app.use(cors())
-app.use(router)
-app.get('/uploads/:id', async (req, res) => {
-    const { id } = req.params
-    const uploads = path.join(__dirname, 'uploads')
-    const files = fs.readdirSync(uploads)
-    const found = files.find(file => file.split('.')[0] == id)
+try {
+    app.use(express.json())
+    app.use(cors())
+    app.use(router)
+    app.get('/uploads/:id', async (req, res) => {
+        const { id } = req.params
+        const uploads = path.join(__dirname, 'uploads')
+        const files = fs.readdirSync(uploads)
+        const found = files.find(file => file.split('.')[0] == id)
 
-    if (!found) {
-        return res.status(404).send("Archivo no encontrado")
-    }
-
-    const filePath = path.join(uploads, found)
-
-    // Obtiene la extensión del archivo para establecer el tipo de contenido
-    const extension = path.extname(filePath)
-    const contentType = `image/${extension.slice(1)}`
-
-    // Lee el contenido del archivo
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            console.error(err)
-            return res.status(500).send("Error al leer el archivo")
+        if (!found) {
+            return res.status(404).send("Archivo no encontrado")
         }
 
-        // Establece el tipo de contenido y envía los datos de la imagen como respuesta
-        res.setHeader('Content-Type', contentType)
-        res.send(data)
+        const filePath = path.join(uploads, found)
+
+        // Obtiene la extensión del archivo para establecer el tipo de contenido
+        const extension = path.extname(filePath)
+        const contentType = `image/${extension.slice(1)}`
+
+        // Lee el contenido del archivo
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).send("Error al leer el archivo")
+            }
+
+            // Establece el tipo de contenido y envía los datos de la imagen como respuesta
+            res.setHeader('Content-Type', contentType)
+            res.send(data)
+        })
     })
-})
-
-
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
-
-
-app.listen(process.env.APP_PORT, () => console.log(`Listening on port ${process.env.APP_PORT}`))
+    // app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+    app.listen(process.env.APP_PORT, () => console.log(`Listening on port ${process.env.APP_PORT}`))
+} catch (err) {
+    console.log('Crash at ' + new Date() + ' with error: ' + err)
+}
